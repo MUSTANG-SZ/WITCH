18f4f4c8ad3c39f829af9e66433efcd473568773
Saianeesh Keshav Haridas <accounts@saianeesh.com>
2022-10-31 12:59:59 -0400

    PEP8 and docstrings

--- minkasi_jax/gauss.py
+++ minkasi_jax/gauss.py
@@ -1,78 +1,106 @@
-import numpy as np
-
+"""
+Gaussian stuff
+"""
+from functools import partial
 import jax
 import jax.numpy as jnp
-JAX_DEBUG_NANS = True
 from jax.config import config
-import jax.scipy.optimize as sopt
-
-from functools import partial
 
 config.update("jax_debug_nans", True)
 config.update("jax_enable_x64", True)
-jax.config.update('jax_platform_name','cpu')
-
-from astropy.cosmology import Planck15 as cosmo
-from astropy import constants as const
-from astropy import units as u
+jax.config.update("jax_platform_name", "cpu")
 
 
+def elliptical_gauss(p, x, y):
+    """
+    Gives the value of an eliptical gaussian.
 
-def eliptical_gauss(p, x, y):
-    #Gives the value of an eliptical gaussian with its center at x0, y0, evaluated at x,y, where theta1 and theta2 are the
-    #FWHM of the two axes and psi is the roation angle. Amp is the amplitude
-    #Note x,y are at the end to make the gradient indicies make more sense i.e. grad(numarg = 0) is grad wrt x0
+    Arguments:
 
-    x0,y0,theta1,theta2,psi,amp = p
+        p: Parameter array (x0, y0, theta1, theta2, psi, amp)
 
-    theta1_inv=1/theta1
-    theta2_inv=1/theta2
-    theta1_inv_sqr=theta1_inv**2
-    theta2_inv_sqr=theta2_inv**2
-    cosdec=jnp.cos(y0)
-    sindec=jnp.sin(y0)/jnp.cos(y0)
-    cospsi=jnp.cos(psi)
-    cc=cospsi**2
-    sinpsi=jnp.sin(psi)
-    ss=sinpsi**2
-    cs=cospsi*sinpsi
+           * x0: Center in x
+           * y0: Center in y
+           * theta1: FWHM along x
+           * theta2: FWHM along y
+           * psi: Rotation angle
+           * amp: Amplitude of gaussian
 
-    delx=(x-x0)*cosdec
-    dely=y-y0
-    xx=delx*cospsi+dely*sinpsi
-    yy=dely*cospsi-delx*sinpsi
-    xfac=theta1_inv_sqr*xx*xx
-    yfac=theta2_inv_sqr*yy*yy
-    rr=xfac+yfac
-    rrpow=jnp.exp(-0.5*rr)
+        x: X values to evaluate the gaussian at
 
-    return amp*rrpow
+        y: Y values to evaluate the gaussian at
+
+    Returns:
+
+        gauss: Elliptical gaussian evaluated at x and y
+    """
+    x0, y0, theta1, theta2, psi, amp = p
+
+    theta1_inv = 1 / theta1
+    theta2_inv = 1 / theta2
+    theta1_inv_sqr = theta1_inv**2
+    theta2_inv_sqr = theta2_inv**2
+    cosdec = jnp.cos(y0)
+    sindec = jnp.sin(y0) / jnp.cos(y0)
+    cospsi = jnp.cos(psi)
+    cc = cospsi**2
+    sinpsi = jnp.sin(psi)
+    ss = sinpsi**2
+    cs = cospsi * sinpsi
+
+    delx = (x - x0) * cosdec
+    dely = y - y0
+    xx = delx * cospsi + dely * sinpsi
+    yy = dely * cospsi - delx * sinpsi
+    xfac = theta1_inv_sqr * xx * xx
+    yfac = theta2_inv_sqr * yy * yy
+    rr = xfac + yfac
+    rrpow = jnp.exp(-0.5 * rr)
+
+    return amp * rrpow
+
 
 @jax.jit
 def gauss(p, x, y):
-    #Gives the value of an eliptical gaussian with its center at x0, y0, evaluated at x,y, where theta1 and theta2 are the
-    #FWHM of the two axes and psi is the roation angle. Amp is the amplitude
-    #Note x,y are at the end to make the gradient indicies make more sense i.e. grad(numarg = 0) is grad wrt x0
+    """
+    Gives the value of an eliptical gaussian.
 
-    x0,y0,sigma,amp = p
+    Arguments:
 
-    sigma_inv=1/sigma
-    sigma_inv_sqr=sigma_inv**2
-    cosdec=jnp.cos(y0)
-    sindec=jnp.sin(y0)/jnp.cos(y0)
+        p: Parameter array (x0, y0, sigma, amp)
+
+           * x0: Center in x
+           * y0: Center in y
+           * sigma: Sigma of gaussian
+           * amp: Amplitude of gaussian
+
+        x: X values to evaluate the gaussian at
+
+        y: Y values to evaluate the gaussian at
+
+    Returns:
+
+        gauss: Elliptical gaussian evaluated at x and y
+    """
+    x0, y0, sigma, amp = p
+
+    sigma_inv = 1 / sigma
+    sigma_inv_sqr = sigma_inv**2
+    cosdec = jnp.cos(y0)
+    sindec = jnp.sin(y0) / jnp.cos(y0)
     cospsi = 1
-    cc=1
-    sinpsi=0
-    ss=sinpsi**2
-    cs=cospsi*sinpsi
+    cc = 1
+    sinpsi = 0
+    ss = sinpsi**2
+    cs = cospsi * sinpsi
 
-    delx=(x-x0)*cosdec
-    dely=y-y0
-    xx=delx*cospsi+dely*sinpsi
-    yy=dely*cospsi-delx*sinpsi
-    xfac=sigma_inv_sqr*xx*xx
-    yfac=sigma_inv_sqr*yy*yy
-    rr=xfac+yfac
-    rrpow=jnp.exp(-0.5*rr)
+    delx = (x - x0) * cosdec
+    dely = y - y0
+    xx = delx * cospsi + dely * sinpsi
+    yy = dely * cospsi - delx * sinpsi
+    xfac = sigma_inv_sqr * xx * xx
+    yfac = sigma_inv_sqr * yy * yy
+    rr = xfac + yfac
+    rrpow = jnp.exp(-0.5 * rr)
 
-    return amp*rrpow
+    return amp * rrpow
