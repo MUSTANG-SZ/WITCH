ab53595517fb89ae6dbfd384ef69c5cc44bcdd14
John Orlowski-Scherer <jorlo@nia-login06.scinet.local>
2023-10-31 17:19:09 -0400

    beginning quicker get_chis

--- minkasi_jax/forward_modeling.py
+++ minkasi_jax/forward_modeling.py
@@ -11,6 +11,61 @@ from minkasi_jax.utils import make_grid
 
 import functools
 
+def faster_get_chis(m, A, rhs, v, weight, dd = None):
+    """
+    A faster, but more importantly much less memory intensive, way to get chis. 
+    The idea is chi^2 = (d-Am)^T N^-1 (d-Am). Previously we would calculate the residuals d-Am 
+    and calculate directly. However d-Am has shape [ndet, nsamp], which is very big. m has shape
+    [nx, ny], much smaller. A, the pointing reconstruction, can be encapsulated into a few pars.
+    Therefore if we can do everthing in map shape, we save a lot on the ammount of stuff we 
+    need to put on the GPU. We can expand chi^2 as 
+
+    d^T N^-1 d -2d^T N^-1 A m + m^T A^T N^-1 A m = dd - 2*dot(m, rhs) + mm
+
+    the first term only has to do with the data. If we care about the absolute value of chi2,
+    which we do at the end, then we can include it in calculation. For MCMC however, we only
+    care about the relative delta chi2 between models. So we can drop that term. For the other
+    terms;
+
+    mm = m^T A^T N^-1 A m
+
+    This term is essentially what we've been doing before, except that m is now in map shape,
+    whereas before m was in tod shape so we essentially had Am. So we need to do Am, but this is
+    in general fast and Jon has a very fast way of doing it. Finally rhs need only be computed
+    once, and while it is an additional thing to put on the gpu it is small, map shape. 
+
+    Parameters
+    ----------
+    m : NDArray[np.floating]
+        The model evaluated at all the map pixels
+    A : NDArray[np.floating]
+        Currently the pointing matrix but hopefully this can be reduced to just some parameters
+        with pointing recontstructed on GPU
+    rhs : NDArray[np.floating]
+        The map output of todvec.make_rhs. Note this is how the data enters into the chi2 calc.
+    v : NDArray[np.floating]
+        The right singular vectors for the noise SVD. These rotate the data into the basis of 
+        the SVD.
+    weight : NDArray[np.floating]
+        The noise weights, in fourier space, SVD decomposed. 
+    dd : None | np.floating
+        Optional chi2 from dd. Not necessary for MCMC but is important for evaluating goodness
+        of fit.
+
+    Outputs
+    -------
+    chi2 : np.floating
+        The chi2 of the model m to the data.
+    """
+    Am = jnp.dot(A, m)
+    Am = Am.at[:,0].set((jnp.sqrt(0.5)*Am)[:,0])
+    Am = Am.at[:,-1].set((jnp.sqrt(0.5)*Am)[:,-1])
+    Am_rot = jnp.dot(v,Am)
+    tmp=jnp.hstack([Am_rot,jnp.fliplr(Am_rot[:,1:-1])]) #mirror pred so we can do dct of first kind
+    predft=jnp.real(jnp.fft.rfft(tmp,axis=1))
+    nn=predft.shape[1]
+    chisq = jnp.sum(weight[:,:nn]*predft**2)
+
 def sampler(params, tods, jsample, model_params, xyz, beam, fixed_pars, fix_pars_ids):
     _params = np.zeros(len(params) + len(fixed_pars))
  
