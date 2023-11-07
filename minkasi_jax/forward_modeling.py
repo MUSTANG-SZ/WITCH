dcf9f180176162bb486e76b04bd17d1b3ae273e3
Sulla2012 <jackorlowskischerer@gmail.com>
2023-11-07 17:17:01 -0500

    Updating get_chis to faster version

--- minkasi_jax/forward_modeling.py
+++ minkasi_jax/forward_modeling.py
@@ -9,9 +9,13 @@ import numpy as np
 from minkasi_jax.core import model
 from minkasi_jax.utils import make_grid
 
+import minkasi.maps.skymap as skymap
+import minkasi.tods.core as todcore
+from minkasi.maps.mapset import Mapset
+
 import functools
 
-def faster_get_chis(m, A, rhs, v, weight, dd = None):
+def faster_get_chis(m, x, y, rhs, v, weight, dd = None):
     """
     A faster, but more importantly much less memory intensive, way to get chis. 
     The idea is chi^2 = (d-Am)^T N^-1 (d-Am). Previously we would calculate the residuals d-Am 
@@ -38,9 +42,10 @@ def faster_get_chis(m, A, rhs, v, weight, dd = None):
     ----------
     m : NDArray[np.floating]
         The model evaluated at all the map pixels
-    A : NDArray[np.floating]
-        Currently the pointing matrix but hopefully this can be reduced to just some parameters
-        with pointing recontstructed on GPU
+    x : NDArray[np.floating]
+        2D array of tod x indicies
+    y : NDArray[np.floating]
+        2D array of tod y indicies
     rhs : NDArray[np.floating]
         The map output of todvec.make_rhs. Note this is how the data enters into the chi2 calc.
     v : NDArray[np.floating]
@@ -57,14 +62,19 @@ def faster_get_chis(m, A, rhs, v, weight, dd = None):
     chi2 : np.floating
         The chi2 of the model m to the data.
     """
-    Am = jnp.dot(A, m)
-    Am = Am.at[:,0].set((jnp.sqrt(0.5)*Am)[:,0])
-    Am = Am.at[:,-1].set((jnp.sqrt(0.5)*Am)[:,-1])
-    Am_rot = jnp.dot(v,Am)
-    tmp=jnp.hstack([Am_rot,jnp.fliplr(Am_rot[:,1:-1])]) #mirror pred so we can do dct of first kind
+
+    model = jax.scipy.ndimage.map_coordinates(m, (x,y),0).reshape((v.shape[1], -1))
+
+    model = model.at[:,0].set((jnp.sqrt(0.5)*model)[:,0])
+    model = model.at[:,-1].set((jnp.sqrt(0.5)*model)[:,-1])
+    model_rot = jnp.dot(v,model)
+    tmp=jnp.hstack([model_rot,jnp.fliplr(model_rot[:,1:-1])]) #mirror pred so we can do dct of first kind
     predft=jnp.real(jnp.fft.rfft(tmp,axis=1))
     nn=predft.shape[1]
-    chisq = jnp.sum(weight[:,:nn]*predft**2) - 2*jnp.dot(m, rhs)
+    print(rhs.shape)
+    chisq = jnp.sum(weight[:,:nn]*predft**2) - 2*jnp.dot(rhs.ravel(), m.ravel())
+
+    return chisq
     
 def sampler(params, tods, jsample, fixed_pars, fix_pars_ids):
     _params = np.zeros(len(params) + len(fixed_pars))
@@ -80,6 +90,44 @@ def sampler(params, tods, jsample, fixed_pars, fix_pars_ids):
             par_idx += 1
 
     return jsample(_params, tods)
+
+def faster_sample(model_params, xyz, beam, x_map, y_map, params, tods):#, model_params, xyz, beam):
+    """
+    Generate a model realization and compute the chis of that model to data.
+    TODO: model components currently hard coded.
+
+    Arguements:
+
+        tods: Array of tod parameters. See prep tods
+
+        params: model parameters
+
+        model_params: number of each model componant 
+
+        xyz: grid to evaluate model at
+
+        beam: Beam to smooth by
+
+    Returns:
+
+        chi2: the chi2 difference of the model to the tods
+
+    """
+    log_like = 0
+    n_iso, n_gnfw, n_gauss, n_egauss, n_uni, n_expo, n_power, n_power_cos = model_params
+
+    m = model(xyz, n_iso, n_gnfw, n_gauss, n_egauss, n_uni, n_expo, n_power, n_power_cos,
+                     -2.5e-05, beam, x_map, y_map, params)
+
+    for i, tod in enumerate(tods):
+        x, y, rhs, v, weight = tod #unravel tod
+    
+        flags = jnp.where((weight != 0))
+        cut_weight = weight.at[flags].set(1e-30) #Set 0 weights to very small
+        norm = (0.50 * jnp.sum(jnp.log(cut_weight/2.0/jnp.pi))) 
+        log_like += faster_get_chis(m, x, y, rhs, v, weight) / norm 
+
+    return log_like
 
 def sample(model_params, xyz, beam, params, tods):#, model_params, xyz, beam):
     """
@@ -113,7 +161,7 @@ def sample(model_params, xyz, beam, params, tods):#, model_params, xyz, beam):
                      -2.5e-05, beam, idx_tod, idy_tod, params)
     
         pred = pred[id_inv].reshape(dat.shape)
-        #chi2 += jget_chis(dat, pred, v, weight)
+
         norm = (0.50 * jnp.sum(jnp.log(cut_weight/2.0/jnp.pi))) 
         log_like += jget_chis(dat, pred, v, weight) / norm 
 
@@ -155,6 +203,32 @@ def get_chis(dat, pred, v, weight):
 
 jget_chis = jax.jit(get_chis)
 
+def make_tod_stuff_fast(todvec, skymap, lims = None, pixsize =  2.0 / 3600 * np.pi / 180):
+    tods = []
+    if lims == None:
+        lims = todvec.lims()
+    refmap = skymap.copy()
+    
+
+    for i, tod in enumerate(todvec.tods):
+        temp_todvec = todcore.TodVec()
+        temp_todvec.add_tod(tod)
+        mapset = Mapset()
+        refmap.clear()
+        mapset.add_map(refmap)
+        temp_todvec.make_rhs(mapset)
+        
+        todgrid = refmap.wcs.wcs_world2pix(np.array([np.rad2deg(tod.info['dx'].flatten()),
+                                                    np.rad2deg(tod.info['dy'].flatten())]).T,1)
+        di = todgrid[:,0].reshape(tod.get_data_dims()).flatten()
+        dj = todgrid[:,1].reshape(tod.get_data_dims()).flatten()
+        #un wrap stuff cause jit doesn't like having tod objects
+        tods.append([jnp.array(di),
+                     jnp.array(dj),
+                     jnp.array(mapset.maps[0].map),
+                     jnp.array(tod.noise.v),
+                     jnp.array(tod.noise.mywt)])
+    return tods
 
 def make_tod_stuff(todvec):
     """
