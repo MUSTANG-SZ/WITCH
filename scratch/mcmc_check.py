fix: fixing dr/r_map units

scratch/mcmc_check.py
@@ -78,6 +78,8 @@ xyz = make_grid(r_map, dr)
 coord_conv = eval(str(cfg["coords"]["conv_factor"]))
 x0 = eval(str(cfg["coords"]["x0"]))
 y0 = eval(str(cfg["coords"]["y0"]))
+print("da: ", da)
+print("dr, r_map: ", dr, r_map)
 
 # Load TODs
 tod_names = glob.glob(os.path.join(cfg["paths"]["tods"], cfg["paths"]["glob"]))
@@ -125,7 +127,27 @@ for i, fname in enumerate(tod_names):
 
 lims = todvec.lims()
 pixsize = 2.0 / 3600 * np.pi / 180
-skymap = skymap.SkyMap(lims, pixsize, square=True, multiple = 2)
+skymap = SkyMap(lims, pixsize, square=True, multiple = 2)
+
+dr = pixsize*da * (3600*180/np.pi)
+r_map = skymap.map.shape[1]*dr/2
+xyz = make_grid(r_map, dr)
+print("dr, r_map: ", dr, r_map)
+#Remake idx/idy after getting maplims
+print("map, xyz: ", skymap.map.shape, xyz[0].shape)
+for i, tod in enumerate(todvec.tods):
+    if fname == "/scratch/r/rbond/jorlo/MS0735/TS_EaCMS0f0_51_5_Oct_2021/Signal_TOD-AGBT21A_123_03-s20.fits": continue
+    dat = tod.info
+    # Make pixelized RA/Dec TODs
+    idx, idy = tod_to_index(dat["dx"], dat["dy"], x0, y0, r_map, dr, coord_conv)
+    idu, id_inv = np.unique(
+        np.vstack((idx.ravel(), idy.ravel())), axis=1, return_inverse=True
+    )
+    dat["idx"] = idu[0]
+    dat["idy"] = idu[1]
+    dat["id_inv"] = id_inv
+    dat["model_idx"] = idx
+    dat["model_idy"] = idy
 
 Te = eval(str(cfg["cluster"]["Te"]))
 freq = eval(str(cfg["cluster"]["freq"]))
@@ -181,7 +203,7 @@ for i, tod in enumerate(todvec.tods):
     print(tod.info["fname"])
     ipix = skymap.get_pix(tod)
     tod.info["ipix"] = ipix
-    
+    print(tod.info["idx"], tod.info["idy"]) 
     if sim:
         tod.info["dat_calib"] *= (-1) ** ((parallel.myrank + parallel.nproc * i) % 2)
         start = 0
@@ -226,7 +248,7 @@ nwalkers, ndim = params2.shape
 
 
 
-
+'''
 sampler = emcee.EnsembleSampler(
     nwalkers, ndim, log_probability, args = (tods, jsample, fixed_params, fixed_pars_ids) #comma needed to not unroll tods
 )
@@ -252,4 +274,4 @@ fig = corner.corner(
 plt.savefig('/scratch/r/rbond/jorlo/sampler/1gauss_corner.pdf')
 plt.savefig('/scratch/r/rbond/jorlo/sampler/1gauss_corner.png')
 
-
+'''