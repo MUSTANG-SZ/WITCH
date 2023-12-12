f7a0339fea25be4994d1eec271a6eb489dde92a2
John Orlowski-Scherer <jorlo@nia-login05.scinet.local>
2023-12-12 13:59:10 -0500

    Moving a10 to its own function in structure

--- minkasi_jax/structure.py
+++ minkasi_jax/structure.py
@@ -8,12 +8,8 @@ import jax
 import jax.numpy as jnp
 from .utils import transform_grid, ap, h70, get_nz, get_hz
 
-
-@partial(
-    jax.jit,
-    static_argnums=(14),
-)
-def gnfw(dx, dy, dz, r_1, r_2, r_3, theta, P0, c500, m500, gamma, alpha, beta, z, fix_r500, xyz):
+@jax.jit
+def gnfw(dx, dy, dz, r_1, r_2, r_3, theta, P0, c500, m500, gamma, alpha, beta, z, xyz):
     """
     Elliptical gNFW pressure profile in 3d.
     This function does not include smoothing or declination stretch
@@ -51,24 +47,83 @@ def gnfw(dx, dy, dz, r_1, r_2, r_3, theta, P0, c500, m500, gamma, alpha, beta, z
 
         xyz: Coordinte grid to calculate model on
 
-        fix_r500: If True, overrides r_1/r_2/r_3 to be equal to the r500 value associated with m500
-
     Returns:
 
         model: The gnfw model
     """
     nz = get_nz(z)
     hz = get_hz(z)
-    if fix_r500:
-        da = get_da(z)
 
     x, y, z = transform_grid(dx, dy, dz, r_1, r_2, r_3, theta, xyz)
 
     r500 = (m500 / (4.00 * jnp.pi / 3.00) / 5.00e02 / nz) ** (1.00 / 3.00)
 
-    if fix_r500:
-        r_1, r_2, r_3 = r500 / da, r500 / da, r500 / da
+    r = c500 * jnp.sqrt(x**2 + y**2 + z**2) / r500
+    denominator = (r**gamma) * (1 + r**alpha) ** ((beta - gamma) / alpha)
 
+    P500 = (
+        1.65e-03
+        * (m500 / (3.00e14 / h70)) ** (2.00 / 3.00 + ap)
+        * hz ** (8.00 / 3.00)
+        * h70**2
+    )
+
+    return P500 * P0 / denominator
+
+@jax.jit
+def a10(dx, dy, dz, theta, P0, c500, m500, gamma, alpha, beta, z, xyz):
+    """
+    Elliptical gNFW pressure profile in 3d based on Arnaud2010.
+    Compared to the function gnfw, this function fixes r1/r2/r3 to r500.
+    This function does not include smoothing or declination stretch
+    which should be applied at the end.
+
+    Arguments:
+
+        dx: RA of cluster center relative to grid origin
+
+        dy: Dec of cluster center relative to grid origin
+
+        dz: Line of sight offset of cluster center relative to grid origin
+
+        r_1: Amount to scale along x-axis
+
+        r_2: Amount to scale along y-axis
+
+        r_3: Amount to scale along z-axis
+
+        theta: Angle to rotate in xy-plane
+
+        P0: Amplitude of the pressure profile
+
+        c500: Concentration parameter at a density contrast of 500
+
+        m500: Mass at a density contrast of 500
+
+        gamma: The central slope
+
+        alpha: The intermediate slope
+
+        beta: The outer slope
+
+        z: Redshift of cluster
+
+        xyz: Coordinte grid to calculate model on
+
+    Returns:
+
+        model: The gnfw model
+    """
+
+    nz = get_nz(z)
+    hz = get_hz(z)
+    da = get_da(z) #TODO pass these arguments rather than recompute them everytime???
+
+    r_1, r_2, r_3 = r500 / da, r500 / da, r500 / da
+
+    x, y, z = transform_grid(dx, dy, dz, r_1, r_2, r_3, theta, xyz)
+
+    r500 = (m500 / (4.00 * jnp.pi / 3.00) / 5.00e02 / nz) ** (1.00 / 3.00)
     r = c500 * jnp.sqrt(x**2 + y**2 + z**2) / r500
     denominator = (r**gamma) * (1 + r**alpha) ** ((beta - gamma) / alpha)
 
