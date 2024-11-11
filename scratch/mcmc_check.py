d17f02753fe47aa07fddc8c23653f89b3ebd55e0
Sulla2012 <jackorlowskischerer@gmail.com>
2024-11-11 16:24:37 -0500

    fix: squash rebase bugs

--- scratch/mcmc_check.py
+++ scratch/mcmc_check.py
@@ -13,7 +13,7 @@ import minkasi.parallel as parallel
 import minkasi.tods.core as mtods
 import minkasi.tods.io as io
 import minkasi.tods.processing as tod_processing
-import minkasi_jax.presets_by_source as pbs
+from minkasi.tools import presets_by_source as pbs
 import numpy as np
 import yaml
 from astropy import units as u
@@ -24,24 +24,22 @@ from minkasi.mapmaking import noise
 import minkasi
 
 from minkasi.maps.skymap import SkyMap
-from minkasi_jax.core import helper
-from minkasi_jax.core import model as mink_model
-from minkasi_jax.forward_modeling import make_tod_stuff, sample
-from minkasi_jax.forward_modeling import sampler as my_sampler
-from minkasi_jax.utils import *
+from witch.forward_modeling import make_tod_stuff, sample
+from witch.forward_modeling import sampler as my_sampler
+from witch.utils import *
 
 import minkasi
 
 from astropy.coordinates import Angle
 from astropy import units as u
 
-import minkasi_jax.presets_by_source as pbs
-from minkasi_jax.utils import *
-from minkasi_jax.core import model as mink_model
-from minkasi_jax.forward_modeling import make_tod_stuff, sample
-from minkasi_jax.forward_modeling import sampler as my_sampler
-from minkasi_jax.fitter import get_outdir, make_parser, load_tods, process_tods, load_config
-from minkasi_jax.containers import Model
+from witch.utils import *
+from witch.core import model as mink_model
+from witch.grid import make_grid_from_wcs
+from witch.forward_modeling import make_tod_stuff, sample
+from witch.forward_modeling import sampler as my_sampler
+from witch.fitter import get_outdir, _make_parser, load_tods, process_tods, load_config
+from witch.containers import Model
 
 import emcee
 import functools
@@ -50,7 +48,7 @@ import functools
 # %load_ext autoreload
 # %autoreload 2
 
-"""
+
 def log_prior(theta):
     dx, dy, dz, r1, r2, r3, theta_1, beta_1, amp_1 = theta
     if np.abs(dx) < 20 and np.abs(dy) < 20 and np.abs(dz) <20 and 0 < r1 < 1 and 0 < r2 < 1 and 0 < r3 < 1 and 0 < theta_1 < 2*np.pi and 0 < beta_1 < 2 and 0 < amp_1 < 1e6:
@@ -62,11 +60,8 @@ def log_probability(theta, tods):
     if not np.isfinite(lp):
         return -np.inf
     return lp + my_sampler(theta, tods)
-"""
 
 
-
-<<<<<<< HEAD
 def log_prior(theta, da):
     dx, dy, sigma, amp_1 = theta
     if (
@@ -92,8 +87,8 @@ def log_prior(theta):
 def log_probability(theta, tods, jsample, fixed_params, fixed_pars_ids):
     lp = log_prior(theta)    
 
-=======
-'''
+
+
 def log_prior(theta):
     dx, dy, sigma, amp_1 = theta
     if np.abs(dx) < 20 and np.abs(dy) < 20 and 1 < sigma < 8 and -10 < amp_1 < 10: 
@@ -102,36 +97,28 @@ def log_prior(theta):
 
 def log_probability(theta, tods, jsample, fixed_params, fixed_pars_ids):
     lp = log_prior(theta)    
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
     if not np.isfinite(lp):
         return -np.inf
     return lp + my_sampler(theta, tods, jsample, fixed_params, fixed_pars_ids)
 
-<<<<<<< HEAD
 
-
-# with open('/home/r/rbond/jorlo/dev/minkasi_jax/configs/sampler_sims/1gauss.yaml', "r") as file:
+#path = "/home/jorlo/dev/minkasi_jax/configs/sampler_sims/"
+#with open(path + '/1gauss.yaml', "r") as file:
 #    cfg = yaml.safe_load(file)
-# with open('/home/jack/dev/minkasi_jax/configs/ms0735/ms0735.yaml', "r") as file:
-#    cfg = yaml.safe_load(file)
-
-
-=======
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
-path = "/home/jorlo/dev/minkasi_jax/configs/sampler_sims/"
-with open(path + '/1gauss.yaml', "r") as file:
-    cfg = yaml.safe_load(file)
 # fit = True
 
-parser = make_parser()
+parser = _make_parser()
 args = parser.parse_args()
 
 # TODO: Serialize cfg to a data class (pydantic?)
 cfg = load_config({}, args.config)
 cfg["fit"] = cfg.get("fit", "model" in cfg)
 cfg["sim"] = cfg.get("sim", False)
+cfg["wnoise"] = cfg.get("wnoise", False)
 cfg["map"] = cfg.get("map", True)
 cfg["sub"] = cfg.get("sub", True)
+if args.wnoise:
+    cfg["wnoise"] = True
 if args.nosub:
     cfg["sub"] = False
 if args.nofit:
@@ -139,8 +126,6 @@ if args.nofit:
 
 # Get TODs
 todvec = load_tods(cfg)
-<<<<<<< HEAD
-=======
 
 # make a template map with desired pixel size an limits that cover the data
 # todvec.lims() is MPI-aware and will return global limits, not just
@@ -150,39 +135,34 @@ pixsize = 2.0 / 3600 * np.pi / 180
 skymap = minkasi.maps.SkyMap(lims, pixsize)
 
 # Define the model and get stuff setup for minkasi
-model = Model.from_cfg(cfg)
-funs = [model.minkasi_helper]
-params = np.array(model.pars)
-npars = np.array([len(params)])
-prior_vals = model.priors
-priors = [None if prior is None else "flat" for prior in prior_vals]
+if "model" in cfg:
+    model = Model.from_cfg(cfg, pix_size=pixsize, lims=tuple(lims))
+else:
+    model = None
+    print_once("No model defined, setting fit, sim, and sub to False")
+    cfg["fit"] = False
+    cfg["sim"] = False
+    cfg["sub"] = False
 
 # Deal with bowling and simming in TODs and setup noise
 noise_class = eval(str(cfg["minkasi"]["noise"]["class"]))
 noise_args = eval(str(cfg["minkasi"]["noise"]["args"]))
 noise_kwargs = eval(str(cfg["minkasi"]["noise"]["kwargs"]))
-bowl_str = process_tods(
-    args, cfg, todvec, skymap, noise_class, noise_args, noise_kwargs, model
-)
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
 
-lims = todvec.lims()
-pixsize = 2.0 / 3600 * np.pi / 180
-skymap = SkyMap(lims, pixsize, square=True, multiple = 2)
-xyz = make_grid_from_skymap(skymap, cfg["coords"]["r_map"], cfg["coords"]["dr"])
+process_tods(cfg, todvec, skymap, noise_class, noise_args, noise_kwargs, model)
+
+xyz = make_grid_from_wcs(skymap.wcs, skymap.map.shape[0], skymap.map.shape[1], 0.00116355, 0.00000969,)
 model.xyz = xyz
-print("skymap, xyz: ", skymap.map.shape, model.xyz[0].shape)
-<<<<<<< HEAD
-
-=======
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
+print("skymap, xyz: ", skymap.map.shape, model.xyz[0].shape, model.xyz[1].shape)
 
 tods = make_tod_stuff(todvec, skymap)
 
 # test_params = params[:13] #for speed only considering single isobeta model
 
-truths = params
-
+params = np.array(model.pars)
+npars = np.array([len(params)])
+truths = np.array(model.pars) #make a copy of params to keep
+print(params)
 model_params = [0, 0, 1, 0, 0, 0, 0, 0]
 
 fixed_pars_ids = []
@@ -194,10 +174,7 @@ params2[:, 2] = np.abs(params2[:, 2])  # Force sigma positive
 print(params2[:,2])
 #jit partial-d sample function
 cur_sample = functools.partial(sample, model)
-<<<<<<< HEAD
-
-=======
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
+cur_sample(params, tods)
 jsample = jax.jit(cur_sample)
 
 nwalkers, ndim = params2.shape
@@ -213,21 +190,18 @@ sampler = emcee.EnsembleSampler(
 )
 
 sampler.run_mcmc(params2, 1000, skip_initial_state_check = True, progress=True)
-<<<<<<< HEAD
 
-=======
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
 
 
 flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
 
 import pickle as pk
 
-<<<<<<< HEAD
-dir = "./"#'/scratch/r/rbond/jorlo/forward-modeling/'
-=======
+
+
+
 odir = "./"#'/scratch/r/rbond/jorlo/forward-modeling/'
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
+
 
 import corner
 truths = params
@@ -240,12 +214,5 @@ plt.savefig(odir+'1gauss_corner.png')
 
 with open(odir+'mcmc_samples.pk', 'wb') as f:
     pk.dump(flat_samples, f)
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 43883e3bbf287cc28dff7707784b612c8dfb3b86
-
-
 plt.savefig(odir + "1gauss_corner.pdf")
 plt.savefig(odir + "1gauss_corner.png")
