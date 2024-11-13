140d2508a26bbf1a08d3cb9367746a2195878c46
Sulla2012 <jackorlowskischerer@gmail.com>
2024-11-13 17:35:05 -0500

    feat: adding piecewise powerlaw

--- witch/nonparametric.py
+++ witch/nonparametric.py
@@ -1,7 +1,7 @@
 import warnings 
 import numpy as np
 
-import .utils as wu
+from . import utils as wu
 
 def bin_map(hdu, rbins, x0=None, y0=None, cunit=None):
     """
@@ -49,8 +49,8 @@ def bin_map(hdu, rbins, x0=None, y0=None, cunit=None):
         warnings.warn("Warning: non-square pixels: RA: {} Dec{}".format(np.abs(hdu[0].header["CDELT1"]), np.abs(hdu[0].header["CDELT2"])))
 
     #The offset is redundent if the binning center is taken to be the map center but frequently it is not
-    x = np.linspace(-hdu[0].data.shape[1]/2*pixsize + hdu[0].header["CRVAL2"] / pixunits, hdu[0].data.shape[1]/2*pixsize + hdu[0].header["CRVAL2"] / pixunits, hdu[0].data.shape[1])
-    y = np.linspace(-hdu[0].data.shape[0]/2*pixsize + hdu[0].header["CRVAL1"] / pixunits, hdu[0].data.shape[0]/2*pixsize + hdu[0].header["CRVAL1"] / pixunits, hdu[0].data.shape[0])
+    x = np.linspace(-hdu[0].data.shape[1]/2*pixsize + hdu[0].header["CRVAL1"] / pixunits, hdu[0].data.shape[1]/2*pixsize + hdu[0].header["CRVAL1"] / pixunits, hdu[0].data.shape[1])
+    y = np.linspace(-hdu[0].data.shape[0]/2*pixsize + hdu[0].header["CRVAL2"] / pixunits, hdu[0].data.shape[0]/2*pixsize + hdu[0].header["CRVAL2"] / pixunits, hdu[0].data.shape[0])
     
     X, Y = np.meshgrid(x, y)
     R = np.sqrt((X-x0)**2+(Y-y0)**2)
@@ -64,3 +64,81 @@ def bin_map(hdu, rbins, x0=None, y0=None, cunit=None):
         var1d[k] = np.var(pixels)
 
     return bin1d, var1d
+
+def broken_power(rs: jax.Array,
+        rbins: jax.Array,
+        amps: jax.Array,
+        pows:jax.Array,
+        c: float,
+        ) -> jax.Array:
+    """
+    Function which returns a broken powerlaw evaluated at rs.
+
+    Parameters:
+    -----------
+    rs : jax.Array
+        Array of rs at which to compute pl.
+    rbins : jax.Array
+        Array of bin edges for power laws
+    amps : jax.Array
+        Amplitudes of power laws
+    pows : jax.Array                                                                                                                                                                                                                                                                            Exponents of power laws
+    c : float
+        Constant offset for powerlaws
+    """
+    condlist = [(rbins[i] <= rs) & (rs < rbins[i+1]) for i in range(len(pows))]
+    
+    cur_c = c
+    funclist = []
+    def power(x, rbin, cur_amp, cur_pow, c):
+        return cur_amp * (x**cur_pow - rbin**cur_pow) + c
+    for i in range(len(condlist)-1, -1, -1):
+        cur_c += amps[i]*rbins[i+1]**pows[i]
+        funclist.append(functools.partial(power, rbin = rbins[i], cur_amp = amps[i], cur_pow = pows[i], c = cur_c))
+
+def nonpara_power(
+    dx: float,
+    dy: float,
+    dz: float,
+    rbins: jax.Array, 
+    amps: jax.Array, 
+    pows:jax.Array,
+    c: float,
+    z: float,
+    xyz: tuple[jax.Array, jax.Array, jax.Array, float, float],
+) -> jax.Array:
+    """
+    Function which computes 3D pressure of segmented power laws
+
+    Parameters:
+    -----------
+    dx : float
+        RA of cluster center relative to grid origin.
+        Passed to `grid.transform_grid`.
+        Units: arcsec
+    dy : float
+        Dec of cluster center relative to grid origin.
+        Passed to `grid.transform_grid`.
+        Units: arcsec
+    dz : float
+        Line of sight offset of cluster center relative to grid origin.
+        Passed to `grid.transform_grid`.
+        Units: arcsec
+    rbins : jax.Array
+        Array of bin edges for power laws
+    amps : jax.Array
+        Amplitudes of power laws
+    pows : jax.Array 
+        Exponents of power laws
+    c : float
+        Constant offset for powerlaws
+    z : float,
+        Redshift of cluster
+    xyz : tuple[jax.Array, jax.Array, jax.Array, float, float]
+        Coordinte grid to calculate model on.
+        See `containers.Model.xyz` for details.
+    """
+
+    x, y, z, *_ = transform_grid(dx, dy, dz, 1., 1., 1., 0., xyz)
+    r = jnp.sqrt(x**2 + y**2 + z**2)
+
